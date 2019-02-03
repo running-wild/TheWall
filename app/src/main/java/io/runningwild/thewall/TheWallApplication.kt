dbc227a61df2ceb85c35ab30490ef6ca376249cd
yolanda <yolanda@runningwild.io>
2019-02-03 21:42:11 +0000

    stetho integration

--- app/src/main/java/io/runningwild/thewall/TheWallApplication.kt
+++ app/src/main/java/io/runningwild/thewall/TheWallApplication.kt
@@ -1,16 +1,17 @@
 package io.runningwild.thewall
 
+import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import io.runningwild.thewall.di.DaggerAppComponent
 import timber.log.Timber
-
 
 class TheWallApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        Stetho.initializeWithDefaults(this)
     }
 
     override fun applicationInjector(): AndroidInjector<out TheWallApplication> {
